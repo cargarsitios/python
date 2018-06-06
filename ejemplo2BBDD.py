67c6ff4b0e093f0887e163904be81762f98afa9a
cargarsitios <38952384+cargarsitios@users.noreply.github.com>
2018-06-05 20:00:26 -0400

    Add files via upload

---
+++ ejemplo2BBDD.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+miConexion=sqlite3.connect("PrimeraBase")
+
+miCursor=miConexion.cursor()
+
+#miCursor.execute("CREATE TABLE PRODUCTOS (NOBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
+miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALON', 15, 'DEPORTES')")
+
+miConexion.commit()
+miConexion.close()
